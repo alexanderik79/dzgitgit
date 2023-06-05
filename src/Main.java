4bb386e46d220bad7674465293b5b4a31b97bea9
alexanderik79 <alexanderik79@gmail.com>
2023-06-05 20:01:01 +0200

    ветка dev2

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("основная");   // master branch
 
-        System.out.println("ветка dev1");
-
-        System.out.println("проверка 1");
-
-        System.out.println("ресет сработал");
-
-        System.out.println("проверка amend");
-
-        // revert сделан
-
-        //сквош сделан
+        System.out.println("ветка dev2");
     }
 }
