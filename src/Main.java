53add7496ac318849ec65b73de1e57f4ee1bbbcb
alexanderik79 <alexanderik79@gmail.com>
2023-06-05 20:01:01 +0200

    ветка dev2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("основная");   // master branch
 
-        System.out.println("ветка dev1");
+        System.out.println("ветка dev2");
     }
 }
