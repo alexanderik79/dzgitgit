3336d44c66d1f89932ff75b7653f5962a1393e85
AlexSimenko <simenkoalex2@gmail.com>
2023-06-06 11:38:55 +0300

    Simenko

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("основная");   // master branch
-
+        System.out.println("git is my pain");
 
     }
 }
